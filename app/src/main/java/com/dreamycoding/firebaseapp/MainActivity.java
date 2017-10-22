37002c9761b6258061cb436536757210d3ba9f65
JuyelRana <juyelcse11th@gmail.com>
2017-10-22 03:00:49 -0700

    Database example

--- app/src/main/java/com/dreamycoding/firebaseapp/MainActivity.java
+++ app/src/main/java/com/dreamycoding/firebaseapp/MainActivity.java
@@ -1,15 +1,43 @@
 package com.dreamycoding.firebaseapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView txtView;
+    private FirebaseDatabase database;
+    private DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtView = (TextView) findViewById(R.id.txtView);
+
+        database = FirebaseDatabase.getInstance();
+        reference = database.getReference("message");
+        reference.setValue("Hello Juyel");
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                txtView.setText(dataSnapshot.getValue(String.class));
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                txtView.setText(databaseError.toException().toString());
+            }
+        });
 
     }
 }
